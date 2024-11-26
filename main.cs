aed9a9ae086ecda4d45874930a224da560a55263
AldenSmith7 <aldenmsmith44@gmail.com>
2024-11-26 01:32:05 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console; 
+
+class Program
+{
+    static void Main()
+    {
+        
+        WriteLine("Oh, well imagine");
+        WriteLine("As I'm pacing the pews in a church corridor");
+        WriteLine("And I can't help but to hear");
+        WriteLine("No, I can't help but to hear an exchanging of words...");
+    }
+}
